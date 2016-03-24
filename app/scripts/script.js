cb209478540b4b17295fb01bbdb07afda93cac24
Joe Fox Jr <joefoxjr@gmail.com>
2016-03-24 19:14:01 -0400

    Remove errant array in js

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-  $('body').css(['background-color','#0066FF']);
-  $('body').css(['color','#100500']);
+  $('body').css('background-color','#0066FF');
+  $('body').css('color','#100500');
 
   for(i=1;i<10;i++){
     console.log(i);
