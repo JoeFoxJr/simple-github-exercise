d9a2195ec633c777d5e94c031fff67a47bda5ca7
Joe Fox Jr <joefoxjr@gmail.com>
2016-03-24 19:50:41 -0400

    switch colors in js file

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-  $('body').css('background-color','#0066FF');
-  $('body').css('color','#100500');
+  $('body').css('background-color','#100500');
+  $('body').css('color','#0066FF');
 
   for(i=1;i<10;i++){
     console.log(i);
